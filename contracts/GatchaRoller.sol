6190cedc173a461df2632fae838341792cc6c9ce
David Voicu <david@voicu.me>
2021-08-03 14:32:25 -0700

    Added NFT

--- contracts/GatchaRoller.sol
+++ contracts/GatchaRoller.sol
@@ -1,6 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.7.0 <0.9.0;
 
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
+
+contract GatchaNFT is NFTokenMetadata, Ownable {
+    /**
+     * @dev Contract constructor. Sets metadata extension `name` and `symbol`.
+     */
+    constructor() {
+        nftName = "Gatcha NFT";
+        nftSymbol = "GFT";
+    }
+
+    /**
+     * @dev Mints a new NFT.
+     * @param _to The address that will own the minted NFT.
+     * @param _tokenId of the NFT to be minted by the msg.sender.
+     * @param _uri String representing RFC 3986 URI.
+     */
+    function mint(
+        address _to,
+        uint256 _tokenId,
+        string calldata _uri
+    ) external onlyOwner {
+        super._mint(_to, _tokenId);
+        super._setTokenUri(_tokenId, _uri);
+    }
+}
+
 contract GatchaRoller {
     // The keyword "public" makes variables
     // accessible from other contracts
@@ -13,6 +41,7 @@ contract GatchaRoller {
     constructor() {
         minter = msg.sender;
         // mint the NFTs and give them to "this"
+        GatchaNFT.mint(msg.sender, _tokenId, _uri);
     }
 
     // Sends an amount of newly created coins to an address
